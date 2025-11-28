df1378b7259938bde7fbfc066e1e794fc3f484fb
Dimitri A <DimitriPilot3@gmail.com>
2025-11-28 21:50:27 +0100

    feat(matchmake-extension): Implement UpdateMatchmakeSessionPart, WIP
    this is a work in progress

---
+++ matchmake-extension/database/update_matchmake_param.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"github.com/PretendoNetwork/nex-go/v2"
+	common_globals "github.com/PretendoNetwork/nex-protocols-common-go/v2/globals"
+)
+
+// UpdateMatchmakeParam updates the MatchmakeParam of a matchmake session
+func UpdateMatchmakeParam(manager *common_globals.MatchmakingManager, gatheringID uint32, matchmakeParam []byte) *nex.Error {
+	result, err := manager.Database.Exec(`UPDATE matchmaking.matchmake_sessions SET matchmake_param=$1 WHERE id=$2`, matchmakeParam, gatheringID)
+	if err != nil {
+		return nex.NewError(nex.ResultCodes.Core.Unknown, err.Error())
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return nex.NewError(nex.ResultCodes.Core.Unknown, err.Error())
+	}
+
+	if rowsAffected == 0 {
+		return nex.NewError(nex.ResultCodes.RendezVous.SessionVoid, "change_error")
+	}
+
+	return nil
+}
